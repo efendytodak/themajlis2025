c7628f043ad4ab906d5ef98b1fe9769193665937
efendytodak <efendy@todak.com>
2025-09-23 14:21:38 +0800

    Updated AllMajlisPage.tsx

--- src/components/AllMajlisPage.tsx
+++ src/components/AllMajlisPage.tsx
@@ -816,7 +816,7 @@ const AllMajlisPage: React.FC = () => {
           ) : (
             <div className="text-center py-8">
               <div className="w-16 h-16 rounded-full mx-auto mb-4 flex items-center justify-center">
-                <img src="/kubahnew.png" alt="kubahnew.png" className="w-10 h-10" />
+                <img src="/iconpng.png" alt="iconpng.png" className="w-10 h-10" />
               </div>
               <h3 className="text-lg font-medium text-gray-900 mb-2">No majlis found</h3>
               <p className="text-gray-500">
